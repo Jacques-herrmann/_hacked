525076a4e233779d8dd1cb13edd6ac2e956f20a3
jacques-lp5 <114427187+jacques-lp5@users.noreply.github.com>
2025-06-05 16:35:00 +0200

    Improve card transformation with perspective for enhanced 3D effect

--- 2_TiltShiningImage/index.js
+++ 2_TiltShiningImage/index.js
@@ -4,7 +4,7 @@ const card = document.querySelector('.reflecting-card__card')
 const reflect = document.querySelector('.reflecting-card__reflect')
 
 const L = 0.08
-const MAX = 40
+const MAX = 30
 const target = {x: 0, y: 0}
 const current = {x: 0, y: 0}
 
@@ -25,7 +25,7 @@ const tick = () => {
     current.x = lerp(current.x, target.x, L)
     current.y = lerp(current.y, target.y, L)
 
-    card.style.transform = `translate(-50%, -50%)  rotateY(${MAX * current.x}deg) rotateX(${-MAX * current.y}deg)`
+    card.style.transform = `perspective(1000px) translate(-50%, -50%)  rotateY(${MAX * current.x}deg) rotateX(${-MAX * current.y}deg)`
     // card.style.transform = `translate(-50%, -50%) rotateX(${-MAX * current.y}deg) rotateY(${MAX * current.x}deg)`
     reflect.style.opacity = 0.5 * Math.abs(current.x)
 }
